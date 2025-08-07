b0992719f5260c512c17566754f699ad0980696b
Isha <ishanaik2k12@gmail.com>
2025-08-07 16:28:27 +1000

    changed public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://52.62.251.45', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
